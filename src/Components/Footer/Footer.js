44f0c98e97ca4ad89e22423e044686bf06db1471
Rizvimahbub <rizvialam007@gmail.com>
2022-10-14 02:36:19 +0600

    last commit

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,15 +6,61 @@ const Footer = () => {
         <div>
             <div>
                 <div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <span>ABOUT</span>
+                        <p>Contact Us</p>
+                        <p>About Us</p>
+                        <p>Careers</p>
+                        <p>Flipkart Stories</p>
+                        <p>Press</p>
+                        <p>Flipkart Wholesale</p>
+                        <p>Corporate Information</p>
+                    </div>
+                    <div>
+                        <span>HELP</span>
+                        <p>Payments</p>
+                        <p>Shipping</p>
+                        <p>Cancellation & Returns</p>
+                        <p>FAQ</p>
+                        <p>Report Infringement</p>
+                    </div>
+                    <div>
+                        <span>POLICY</span>
+                        <p>Return Policy</p>
+                        <p>Terms Of Use</p>
+                        <p>Security</p>
+                        <p>Privacy</p>
+                        <p>Sitemap</p>
+                        <p>EPR Compliance</p>
+                    </div>
+                    <div>
+                        <span>SOCIAL</span>
+                        <p>Facebook</p>
+                        <p>Twitter</p>
+                        <p>YouTube</p>
+                    </div>
                 </div>
-                <div></div>
                 <div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <span>Mail Us:</span>
+                        <p>Flipkart Internet Private Limited,</p>
+                        <p>Buildings Alyssa, Begonia &</p>
+                        <p>Clove Embassy Tech Village,</p>
+                        <p>Outer Ring Road, Devarabeesanahalli Village,</p>
+                        <p>Bengaluru, 560103,</p>
+                        <p>Karnataka, India</p>
+                    </div>
+                    <div>
+                        <span>Registered Office Address:</span>
+                        <p>Flipkart Internet Private Limited,</p>
+                        <p>Buildings Alyssa, Begonia &</p>
+                        <p>Clove Embassy Tech Village,</p>
+                        <p>Outer Ring Road, Devarabeesanahalli Village,</p>
+                        <p>Bengaluru, 560103,</p>
+                        <p>Karnataka, India</p>
+                        <p>CIN : U51109KA2012PTC066107</p>
+                        <p>Telephone: <span>044-45614700</span></p>
+                    </div>
                 </div>
             </div>
             <div></div>
