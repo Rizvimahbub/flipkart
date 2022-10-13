fededcaec2f65b2520d89dd1229257be324c627d
Rizvimahbub <rizvialam007@gmail.com>
2022-10-13 21:57:45 +0600

    first commit

--- src/Components/Explore Plus/SecondNav.js
+++ src/Components/Explore Plus/SecondNav.js
@@ -212,7 +212,7 @@ const SecondNav = () => {
                         </div>
                     </div>
                 </div>
-                <div className=' div-1 grow relative group'>
+                <div className='grow relative group'>
                     <p className='text-sm nav-p group-hover:text-[#1e70f3] text-[#212121] h-[41px] flex items-center justify-center font-semibold'>Men <FontAwesomeIcon className='second-nav-text-icon group-hover:text-[#1e70f3] group-hover:rotate-[-180deg] duration-100 text-[#b2b2b2] ml-1 w-[9px]' icon={faChevronDown} /></p>
                     <div className='absolute top-[40px] w-[1250px] hidden shadow shadow-gray-400 rounded-sm sub-nav-div flex top-[40px] left-[-306px] group-hover:flex'>
                         <div className=' grow w-full bg-white rounded-l-sm'>
