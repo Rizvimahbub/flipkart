fa8f54f53258b137186f8e497fd34680a9c722ac
Rizvimahbub <rizvialam007@gmail.com>
2022-10-13 21:24:41 +0600

    women section complete

--- src/Components/Explore Plus/SecondNav.js
+++ src/Components/Explore Plus/SecondNav.js
@@ -213,7 +213,7 @@ const SecondNav = () => {
                     </div>
                 </div>
                 <div className=' div-1 grow relative group'>
-                    <p className='text-sm nav-p text-[#1f1f1f] h-[41px] flex items-center justify-center font-semibold'>Men <FontAwesomeIcon className='second-nav-text-icon duration-100 text-[#b2b2b2] ml-1 w-[9px]' icon={faChevronDown} /></p>
+                    <p className='text-sm nav-p text-[#212121] h-[41px] flex items-center justify-center font-semibold'>Men <FontAwesomeIcon className='second-nav-text-icon duration-100 text-[#b2b2b2] ml-1 w-[9px]' icon={faChevronDown} /></p>
                     <div className='absolute top-[40px] w-[1250px] hidden shadow shadow-gray-400 rounded-sm sub-nav-div flex top-[40px] left-[-306px] group-hover:flex'>
                         <div className=' grow w-full bg-white rounded-l-sm'>
                             <p className='flex font-medium items-center text-[13px] px-5 py-2'>Footwear <img className='w-1.5 h-1.5 ml-2' src={Play} /></p>
