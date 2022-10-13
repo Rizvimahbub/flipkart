b30eb9b472b6728d63003649acc8449c29af0a7e
Rizvimahbub <rizvialam007@gmail.com>
2022-10-13 22:55:24 +0600

    last commit

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowTrendUp, faBell, faCaretUp, faCartShopping, faChevronDown, faCircleUser, faGift, faHeart, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import Plus from './Navbar Images/flipkart_star-removebg-preview__1_-removebg-preview-removebg-preview-removebg-preview.png';
 import Orders from './Navbar Images/Orders-removebg-preview (2).png';
@@ -9,6 +9,7 @@ import Message from './Navbar Images/Message-question.png';
 import Download from './Navbar Images/Download.png';
 
 const Navbar = () => {
+    const [sticky, setSticky]  = useState(false);
     return (
         <div className="navbar min-w-[1500px] bg-[#2874F0] z-10">
             <div className='flex justify-around mx-auto min-w-[700px]'>
