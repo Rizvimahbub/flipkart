59a8d21bac6471a10587fcbf14c9f0fa86ff26be
Rizvimahbub <rizvialam007@gmail.com>
2022-10-14 02:46:51 +0600

    last commit

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,62 +4,62 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className='bg-[#172337]'>
-            <div>
-                <div>
+            <div className='flex'>
+                <div className='flex'>
                     <div>
                         <span>ABOUT</span>
-                        <p>Contact Us</p>
-                        <p>About Us</p>
-                        <p>Careers</p>
-                        <p>Flipkart Stories</p>
-                        <p>Press</p>
-                        <p>Flipkart Wholesale</p>
-                        <p>Corporate Information</p>
+                        <p className='text-white'>Contact Us</p>
+                        <p className='text-white'>About Us</p>
+                        <p className='text-white'>Careers</p>
+                        <p className='text-white'>Flipkart Stories</p>
+                        <p className='text-white'>Press</p>
+                        <p className='text-white'>Flipkart Wholesale</p>
+                        <p className='text-white'>Corporate Information</p>
                     </div>
                     <div>
                         <span>HELP</span>
-                        <p>Payments</p>
-                        <p>Shipping</p>
-                        <p>Cancellation & Returns</p>
-                        <p>FAQ</p>
-                        <p>Report Infringement</p>
+                        <p className='text-white'>Payments</p>
+                        <p className='text-white'>Shipping</p>
+                        <p className='text-white'>Cancellation & Returns</p>
+                        <p className='text-white'>FAQ</p>
+                        <p className='text-white'>Report Infringement</p>
                     </div>
                     <div>
                         <span>POLICY</span>
-                        <p>Return Policy</p>
-                        <p>Terms Of Use</p>
-                        <p>Security</p>
-                        <p>Privacy</p>
-                        <p>Sitemap</p>
-                        <p>EPR Compliance</p>
+                        <p className='text-white'>Return Policy</p>
+                        <p className='text-white'>Terms Of Use</p>
+                        <p className='text-white'>Security</p>
+                        <p className='text-white'>Privacy</p>
+                        <p className='text-white'>Sitemap</p>
+                        <p className='text-white'>EPR Compliance</p>
                     </div>
                     <div>
                         <span>SOCIAL</span>
-                        <p>Facebook</p>
-                        <p>Twitter</p>
-                        <p>YouTube</p>
+                        <p className='text-white'>Facebook</p>
+                        <p className='text-white'>Twitter</p>
+                        <p className='text-white'>YouTube</p>
                     </div>
                 </div>
-                <div>
+                <div className='flex'>
                     <div>
                         <span>Mail Us:</span>
-                        <p>Flipkart Internet Private Limited,</p>
-                        <p>Buildings Alyssa, Begonia &</p>
-                        <p>Clove Embassy Tech Village,</p>
-                        <p>Outer Ring Road, Devarabeesanahalli Village,</p>
-                        <p>Bengaluru, 560103,</p>
-                        <p>Karnataka, India</p>
+                        <p className='text-white'>Flipkart Internet Private Limited,</p>
+                        <p className='text-white'>Buildings Alyssa, Begonia &</p>
+                        <p className='text-white'>Clove Embassy Tech Village,</p>
+                        <p className='text-white'>Outer Ring Road, Devarabeesanahalli Village,</p>
+                        <p className='text-white'>Bengaluru, 560103,</p>
+                        <p className='text-white'>Karnataka, India</p>
                     </div>
                     <div>
                         <span>Registered Office Address:</span>
-                        <p>Flipkart Internet Private Limited,</p>
-                        <p>Buildings Alyssa, Begonia &</p>
-                        <p>Clove Embassy Tech Village,</p>
-                        <p>Outer Ring Road, Devarabeesanahalli Village,</p>
-                        <p>Bengaluru, 560103,</p>
-                        <p>Karnataka, India</p>
-                        <p>CIN : U51109KA2012PTC066107</p>
-                        <p>Telephone: <span>044-45614700</span></p>
+                        <p className='text-white'>Flipkart Internet Private Limited,</p>
+                        <p className='text-white'>Buildings Alyssa, Begonia &</p>
+                        <p className='text-white'>Clove Embassy Tech Village,</p>
+                        <p className='text-white'>Outer Ring Road, Devarabeesanahalli Village,</p>
+                        <p className='text-white'>Bengaluru, 560103,</p>
+                        <p className='text-white'>Karnataka, India</p>
+                        <p className='text-white'>CIN : U51109KA2012PTC066107</p>
+                        <p className='text-white'>Telephone: <span>044-45614700</span></p>
                     </div>
                 </div>
             </div>
