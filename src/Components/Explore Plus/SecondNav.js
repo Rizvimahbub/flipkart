9cfe2dac273e3aafea2e5ef6e163bcd503632f37
Rizvimahbub <rizvialam007@gmail.com>
2022-10-13 21:10:55 +0600

    women section complete

--- src/Components/Explore Plus/SecondNav.js
+++ src/Components/Explore Plus/SecondNav.js
@@ -212,9 +212,9 @@ const SecondNav = () => {
                         </div>
                     </div>
                 </div>
-                <div className=' div-1 grow relative'>
+                <div className=' div-1 grow relative group'>
                     <p className='text-sm nav-p text-[#1f1f1f] h-[41px] flex items-center justify-center font-semibold'>Men <FontAwesomeIcon className='second-nav-text-icon duration-100 text-[#b2b2b2] ml-1 w-[9px]' icon={faChevronDown} /></p>
-                    <div className='absolute top-[40px] w-[1250px] hidden shadow shadow-gray-400 rounded-sm sub-nav-div flex top-[40px] left-[-306px]'>
+                    <div className='absolute top-[40px] w-[1250px] hidden shadow shadow-gray-400 rounded-sm sub-nav-div flex top-[40px] left-[-306px] group-hover:flex'>
                         <div className=' grow w-full bg-white rounded-l-sm'>
                             <p className='flex font-medium items-center text-[13px] px-5 py-2'>Footwear <img className='w-1.5 h-1.5 ml-2' src={Play} /></p>
                             <p className='sub-p text-[#7d7d7d] px-5 py-1.5  text-xs font-medium'>Sports Shoes</p>
@@ -310,9 +310,9 @@ const SecondNav = () => {
                         </div>
                     </div>
                 </div>
-                <div className=' div-1 grow relative'>
+                <div className=' div-1 grow relative group'>
                     <p className='text-sm nav-p text-[#1f1f1f] h-[41px] flex items-center justify-center font-semibold'>Women <FontAwesomeIcon className='second-nav-text-icon duration-100 text-[#b2b2b2] ml-1 w-[9px]' icon={faChevronDown} /></p>
-                    <div className='absolute top-[40px] w-[1250px] hidden  shadow shadow-gray-400 rounded-sm sub-nav-div flex top-[40px] left-[-399px]'>
+                    <div className='group-hover:flex absolute top-[40px] w-[1250px] hidden  shadow shadow-gray-400 rounded-sm sub-nav-div flex top-[40px] left-[-399px]'>
                         <div className=' grow w-full bg-white rounded-l-sm'>
                             <p className='flex font-medium items-center text-[13px] px-5 py-2'>Clothing <img className='w-1.5 h-1.5 ml-2' src={Play} /></p>
                             <p className='flex font-medium items-center text-[13px] px-5 py-2'>Women Western & Maternity Wear</p>
@@ -421,9 +421,9 @@ const SecondNav = () => {
                         </div>
                     </div>
                 </div>
-                <div className=' div-1 grow relative'>
+                <div className=' div-1 grow relative group'>
                     <p className='flex items-center justify-center text-sm nav-p text-[#1f1f1f] h-[41px] font-semibold'>Baby & Kids <FontAwesomeIcon className='second-nav-text-icon duration-100 text-[#b2b2b2] ml-1 w-[9px]' icon={faChevronDown} /></p>
-                    <div className='absolute top-[40px] w-[1250px] hidden shadow shadow-gray-400 rounded-sm sub-nav-div flex top-[40px] left-[-513px]'>
+                    <div className='group-hover:flex absolute top-[40px] w-[1250px] hidden shadow shadow-gray-400 rounded-sm sub-nav-div flex top-[40px] left-[-513px]'>
                         <div className=' grow w-full bg-white rounded-l-sm'>
                             <p className='flex font-medium items-center text-[13px] px-5 py-2'>Kids Clothing <img className='w-1.5 h-1.5 ml-2' src={Play} /></p>
                             <p className='flex font-medium items-center text-[13px] px-5 py-2'>Boys' Clothing <img className='w-1.5 h-1.5 ml-2' src={Play} /></p>
@@ -528,9 +528,9 @@ const SecondNav = () => {
                         </div>
                     </div>
                 </div>
-                <div className=' div-1 grow relative'>
+                <div className=' div-1 grow relative group'>
                     <p className='flex items-center justify-center text-sm nav-p text-[#1f1f1f] h-[41px] font-semibold'>Home & Furniture <FontAwesomeIcon className='second-nav-text-icon duration-100 text-[#b2b2b2] ml-1 w-[9px]' icon={faChevronDown} /></p>
-                    <div className='absolute top-[40px] w-[1250px] hidden shadow shadow-gray-400 rounded-sm sub-nav-div flex top-[40px] left-[-653px]'>
+                    <div className='group-hover:flex absolute top-[40px] w-[1250px] hidden shadow shadow-gray-400 rounded-sm sub-nav-div flex top-[40px] left-[-653px]'>
                         <div className=' grow w-full bg-white rounded-l-sm'>
                             <p className='flex font-medium items-center text-[13px] px-5 py-2'>Kitchen, Cookware & Serveware <img className='w-1.5 h-1.5 ml-2' src={Play} /></p>
                             <p className='sub-p text-[#7d7d7d] px-5 py-1.5  text-xs font-medium'>Pans</p>
