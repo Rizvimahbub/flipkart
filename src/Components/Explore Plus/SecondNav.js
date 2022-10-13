7a3629d1132b1251e6113f1b812d7e966b130e39
Rizvimahbub <rizvialam007@gmail.com>
2022-10-13 21:49:35 +0600

    first commit

--- src/Components/Explore Plus/SecondNav.js
+++ src/Components/Explore Plus/SecondNav.js
@@ -7,7 +7,7 @@ import Play from './Images/Play.png';
 const SecondNav = () => {
     return (
         <div className=' pr-12 shadow-black h-[41px] min-w-[1500px] border-b border-[#D1D3D5]'>
-            <div className=' cursor-pointer second-sub-nav max-w-[1248px] flex items-center w-5/6 mx-auto h-[40px]'>
+            <div className=' cursor-pointer max-w-[1248px] flex items-center w-5/6 mx-auto h-[40px]'>
                 <div className='div-1 grow relative group'>
                     <p className='text-sm nav-p group-hover:text-[#1e70f3] text-[#212121] h-[41px] flex items-center justify-center font-semibold'>Electronics <FontAwesomeIcon className='second-nav-text-icon group-hover:text-[#1e70f3] group-hover:rotate-[-180deg] duration-100 text-[#b2b2b2] ml-1 w-[9px]' icon={faChevronDown} /></p>
                     <div className='absolute top-[40px] w-[1250px] hidden group-hover:flex shadow shadow-gray-400 rounded-sm sub-nav-div flex'>
